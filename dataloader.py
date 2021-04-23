d2d286315e028c4355cdb6787928c3daed481248
unknown <riccardo.cadei98@gmail.com>
2021-04-24 01:09:25 +0200

    plot data and add gitignore

---
+++ dataloader.py
@@ -0,0 +1,101 @@
+
+import torch
+from torchvision import datasets
+
+import argparse
+import sys
+sys.argv = ['']
+import os
+
+import urllib
+
+######################################################################
+
+parser = argparse.ArgumentParser(description='MNIST Data Loader')
+
+parser.add_argument('--full',
+                    action='store_true', default=True,
+                    help = 'Use the full set, can take ages (default False)')
+
+parser.add_argument('--tiny',
+                    action='store_true', default=False,
+                    help = 'Use a very small set for quick checks (default False)')
+
+parser.add_argument('--seed',
+                    type = int, default = 0,
+                    help = 'Random seed (default 0, < 0 is no seeding)')
+
+parser.add_argument('--data_dir',
+                    type = str, default = None,
+                    help = 'Where are the PyTorch data located (default $PYTORCH_DATA_DIR or \'./data\')')
+
+# Timur's fix
+parser.add_argument('-f', '--file',
+                    help = 'quick hack for jupyter')
+
+args = parser.parse_args()
+
+if args.seed >= 0:
+    torch.manual_seed(args.seed)
+
+######################################################################
+# The data
+
+def convert_to_one_hot_labels(input, target):
+    tmp = input.new_zeros(target.size(0), target.max() + 1)
+    tmp.scatter_(1, target.view(-1, 1), 1.0)
+    return tmp
+
+def load_data(one_hot_labels = False, normalize = False, flatten = True):
+
+    if args.data_dir is not None:
+        data_dir = args.data_dir
+    else:
+        data_dir = os.environ.get('PYTORCH_DATA_DIR')
+        if data_dir is None:
+            data_dir = './data'
+
+  
+    print('* Using MNIST')
+
+    mnist_train_set = datasets.MNIST(data_dir + '/mnist/', train = True, download = True)
+    mnist_test_set = datasets.MNIST(data_dir + '/mnist/', train = False, download = True)
+
+    train_input = mnist_train_set.data.view(-1, 1, 28, 28).float()
+    train_target = mnist_train_set.targets
+    test_input = mnist_test_set.data.view(-1, 1, 28, 28).float()
+    test_target = mnist_test_set.targets
+
+    if flatten:
+        train_input = train_input.clone().reshape(train_input.size(0), -1)
+        test_input = test_input.clone().reshape(test_input.size(0), -1)
+
+    if args.full:
+        if args.tiny:
+            raise ValueError('Cannot have both --full and --tiny')
+    else:
+        if args.tiny:
+            print('** Reduce the data-set to the tiny setup')
+            train_input = train_input.narrow(0, 0, 500)
+            train_target = train_target.narrow(0, 0, 500)
+            test_input = test_input.narrow(0, 0, 100)
+            test_target = test_target.narrow(0, 0, 100)
+        else:
+            print('** Reduce the data-set (use --full for the full thing)')
+            train_input = train_input.narrow(0, 0, 1000)
+            train_target = train_target.narrow(0, 0, 1000)
+            test_input = test_input.narrow(0, 0, 1000)
+            test_target = test_target.narrow(0, 0, 1000)
+
+    print('** Use {:d} train and {:d} test samples'.format(train_input.size(0), test_input.size(0)))
+
+    if one_hot_labels:
+        train_target = convert_to_one_hot_labels(train_input, train_target)
+        test_target = convert_to_one_hot_labels(test_input, test_target)
+
+    if normalize:
+        mu, std = train_input.mean(), train_input.std()
+        train_input.sub_(mu).div_(std)
+        test_input.sub_(mu).div_(std)
+
+    return train_input, train_target, test_input, test_target
