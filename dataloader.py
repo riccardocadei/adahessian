8b2cf394e0071c2d1c1603337a6b19e1932f04b4
raphaelattias <raphael.attias@epfl.ch>
2021-05-02 11:00:35 +0200

    Training with Adam on MNIST

--- dataloader.py
+++ dataloader.py
@@ -1,6 +1,8 @@
 
 import torch
 from torchvision import datasets
+from torch.utils.data import TensorDataset, DataLoader
+
 
 import argparse
 import sys
@@ -99,3 +101,30 @@ def load_data(one_hot_labels = False, normalize = False, flatten = True):
         test_input.sub_(mu).div_(std)
 
     return train_input, train_target, test_input, test_target
+
+
+def mean_std(train_loader):
+    """Useful tool to compute the mean and the standard deviation of the train loader. This is used either to check if the data
+    loader is normalized, or to compute the mean and std for the normalizer in data_loader.
+
+    Args:
+        train_loader (Dataloader): Pytorch dataloader
+    """
+    nimages = 0
+    mean = 0.
+    std = 0.
+    for batch, _ in train_loader:
+        # Rearrange batch to be the shape of [B, C, W * H]
+        batch = batch.view(batch.size(0), batch.size(1), -1)
+        # Update total number of images
+        nimages += batch.size(0)
+        # Compute mean and std here
+        mean += batch.mean(2).sum(0) 
+        std += batch.std(2).sum(0)
+
+    # Final step
+    mean /= nimages
+    std /= nimages
+
+    print(mean)
+    print(std)
